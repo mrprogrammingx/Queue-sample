46a7aacf9f81024a6475451953ac7c60adfd7ebc
amir <amirjoun2@gmail.com>
2025-02-26 22:22:03 +0400

    Added AliasServiceProvider for loading aliases

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    App\Providers\AliasServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     Barryvdh\Debugbar\ServiceProvider::class,
